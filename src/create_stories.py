5fc505e6c5040370b328ebec49d323d86ab83d79
Patrik Albert <patrik.albert5@gmail.com>
2023-11-07 20:08:57 +0100

    accept url from template + data types in functions

--- src/create_stories.py
+++ src/create_stories.py
@@ -5,6 +5,7 @@ import os
 from argparse import ArgumentParser
 
 from get_posts_metadata import get_posts_metadata
+from get_posts_metadata import PostData
 
 from canvas import Canvas, ImageElements, Background, Text
 from canvas import create_story
@@ -19,25 +20,33 @@ class Template(BaseModel):
     texts_config: Optional[List]
 
 
-def get_story_template() -> Template:
-    template_path = os.path.join("data", site, "template.yaml")
-    with open(template_path) as template:
-        template_content = yaml.safe_load(template)
+def template_path(site: str) -> str:
+    return os.path.join("data", site, "template.yaml")
+
+
+def get_api_url(site: str) -> str:
+    with open(template_path(site)) as template:
+        template = yaml.safe_load(template)
+    return template["url"]
+
+def get_story_template(site: str) -> Template:
+    with open(template_path(site)) as template:
+        template = yaml.safe_load(template)
         
-        if "texts" in template_content["elements"]:
-            texts_config = template_content["elements"]["texts"]
+        if "texts" in template["elements"]:
+            texts_config = template["elements"]["texts"]
         else: 
             text_config = None
         
         return Template(
-            canvas = template_content["canvas"],
-            elements = template_content["elements"],
-            background = template_content["elements"]["background"],
+            canvas = template["canvas"],
+            elements = template["elements"],
+            background = template["elements"]["background"],
             texts_config = texts_config,
         )
     
     
-def clear_files(site) -> None:
+def clear_files(site: str) -> None:
     output_folder = os.path.join("stories", site)
     for file in os.listdir(output_folder):
         if file != "metadata.yaml":
@@ -96,7 +105,7 @@ def get_post_elements(number: int, post, template) -> ImageElements:
         ))
 
 
-def store_metadata(post, elements):
+def store_metadata(post: PostData, elements: ImageElements) -> Dict:
     texts = [x.text for x in elements.texts]
     return {
         "number": elements.number,
@@ -108,7 +117,7 @@ def store_metadata(post, elements):
     }
 
 
-def write_metadata_file(metadata):
+def write_metadata_file(metadata: List[Dict]) -> None:
     md_file = os.path.join("stories", site, "metadata.yaml")
     if os.path.isfile(md_file):
         os.remove(md_file)
@@ -150,11 +159,10 @@ if __name__ == "__main__":
     
     site = args.site
     clear_files(site)
-    api_url = "https://hashtag.zoznam.sk/wp-json/wp/v2"
-    # api_url = "https://plnielanu.zoznam.sk/wp-json/wp/v2"
-    posts = get_posts_metadata(api_url)
+
+    posts = get_posts_metadata(get_api_url(site))
     
-    story_template = get_story_template()
+    story_template = get_story_template(site)
     metadata = []    
     for number, post in enumerate(posts):
         post_elements = get_post_elements(number, post, story_template)
