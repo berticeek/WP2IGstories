7b4a83c723e02999ebde6f82c9c5fd6cf607a39d
Patrik Albert <patrik.albert5@gmail.com>
2023-11-26 20:55:55 +0100

    superfluous

--- src/static/js/loading_bar.js
+++
@@ -1,8 +0,0 @@
-// source: https://codepen.io/splitti/pen/jLZjgx
-
-const svg = document.getElementById('progress_loader')
-svg.classList.add('progress-loader')
-setInterval(() => {
-  svg.classList.toggle('progress-loader')
-  svg.classList.toggle('ready-loader')
-}, 10000)
