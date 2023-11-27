1f82612f741fcb7ac6f553e1c7678982d4c09eb6
Patrik Albert <patrik.albert5@gmail.com>
2023-11-27 23:36:58 +0100

    add comments

--- src/file_paths.py
+++ src/file_paths.py
@@ -1,3 +1,5 @@
+"""Retrieve absolute paths to the files used among different modules"""
+
 import os
 from pathlib import Path
 
@@ -7,18 +9,23 @@ PROJECT_FOLDER = SCRIPT_FOLDER.parent
 
 
 def project_folder() -> Path:
+    """Project root path"""
     return PROJECT_FOLDER
 
 
 def template_path(site: str) -> str:
+    """Path to stories template file"""
     return PROJECT_FOLDER / "data" / site / "template.yaml"
 
 
 def predef_posts_file(site: str) -> str:
+    """Path to the file with predefined posts links"""
+    # Probably not needed anymore, predefined posts are entered on the web
     return PROJECT_FOLDER / "stories" / site / "stories.yaml"
 
 
 def clear_files(site: str) -> None:
+    """Remove created data"""
     ignore_files = ["metadata.yaml", "stories.yaml"]
     output_folder = PROJECT_FOLDER / "stories" / site
     for file in os.listdir(output_folder):
