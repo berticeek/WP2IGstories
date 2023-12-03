5d37bc0d53dd9d2e4bfbe07bcf9a7e1ab2600fec
Patrik Albert <patrik.albert5@gmail.com>
2023-12-03 11:32:00 +0100

    add return  codes

--- src/app.py
+++ src/app.py
@@ -72,7 +72,7 @@ def get_posts_data():
     site = request.args.get("site")
     if site is None:
         LOG.error("Missing 'site' in the request.")
-        return jsonify({"success": False, "error": "Missing 'site' in the request."})
+        return jsonify({"success": False, "error": "Missing 'site' in the request."}), 400
         
     links = list(filter(None, request.args.getlist("links")[0].split(",")))
     if links:
@@ -88,7 +88,7 @@ def get_posts_data():
         try:
             posts_number = int(posts_number_value)
         except ValueError as ve:
-            return jsonify({"success": False, "error": str(ve)})
+            return jsonify({"success": False, "error": str(ve)}), 400
     
     posts_data = get_posts_metadata(site, links, int(posts_number))
     return jsonify({"success": True, "data": posts_data})
@@ -101,12 +101,12 @@ def get_stories_template():
     site = request.args.get("site")
     if site is None:
         LOG.error("Missing 'site' in the request.")
-        return jsonify({"success": False, "error": "Missing 'site' in the request."})
+        return jsonify({"success": False, "error": "Missing 'site' in the request."}), 400
     
     template = get_story_template(site)
     if not template:
         LOG.error("Error while getting template.")
-        return jsonify({"success": False, "error": "Couldn't load template."})
+        return jsonify({"success": False, "error": "Couldn't load template."}), 500
     
     return jsonify({"success": True, "data": template})
 
@@ -127,10 +127,10 @@ def get_posts_elements():
         
     except ValueError as ve:
         LOG.exception("Error in 'posts' parameter")
-        return jsonify({"success": False, "error": str(ve)})
+        return jsonify({"success": False, "error": str(ve)}), 400
     except Exception as e:
         LOG.exception("Unexpected error occurred")
-        return jsonify({"success": False, "error": "Unexpected error occurred"})
+        return jsonify({"success": False, "error": "Unexpected error occurred"}), 400
     
     try:
         template_value = request.args.get("template")
@@ -144,10 +144,10 @@ def get_posts_elements():
     
     except ValueError as ve:
         LOG.exception("Error in 'template' parameter")
-        return jsonify({"success": False, "error": str(ve)})
+        return jsonify({"success": False, "error": str(ve)}), 400
     except Exception as e:
         LOG.exception("Unexpected error occurred")
-        return jsonify({"success": False, "error": "Unexpected error occurred"})
+        return jsonify({"success": False, "error": "Unexpected error occurred"}), 400
     
     posts_elements = get_elements(posts, template)
     return jsonify({"success": True, "data": posts_elements})
